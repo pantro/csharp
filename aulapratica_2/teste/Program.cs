6dcc8a577cb79fe54ab3209937a20891a6525141
Gian Franco Joel Condori Luna <msft24@cervantes.LABS-IC>
2022-09-03 11:54:08 -0300

    this

--- aulapratica_2/teste/Program.cs
+++ aulapratica_2/teste/Program.cs
@@ -1,34 +1,15 @@
-﻿var rect = new Rectangle(3, 4);              // Criação de objeto do tipo Rectangle
+﻿Panda a = new Panda();
+Panda b = new Panda();
+a.Marry(b);
+//Console.WriteLine(new Panda().Marry(new Panda()));
 
-(float width, float height) = rect;            // Desconstrução
-Console.WriteLine(width + " " + height);    // 3 4
-
-// O uso de tipagem implícita é usada com a palavra-chave var:  
-var (x, y) = rect;                           // Desconstrução
-Console.WriteLine(x + " " + y);
-
-// Se as variáveis já foram declaradas, podemos apenas chamar o desconstrutor
-(x, y) = rect; 
-Console.WriteLine(x + " " + y);
-
-// A partir do C#, é permitido o uso de sintaxe mista
-double x1 = 0;
-(x1, double y2) = rect;
-Console.WriteLine(x + " " + y);
-
-class Rectangle
+public class Panda
 {
-  public readonly float Width, Height;
+  public Panda Mate;
 
-  public Rectangle(float width, float height)
+  public void Marry(Panda partner)
   {
-    Width = width;
-    Height = height;
-  }
-
-  public void Deconstruct(out float width, out float height)    // Declaração do método desconstrutor
-  {
-    width = Width;
-    height = Height;
+    Mate = partner;
+    partner.Mate = this;        // Atribui o próprio objeto
   }
 }
