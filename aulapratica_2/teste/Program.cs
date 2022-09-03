14b47ecef265ea71276776434548a9e3c5a3784f
Gian Franco Joel Condori Luna <msft24@cervantes.LABS-IC>
2022-09-03 10:54:52 -0300

    dotnet new sln, dotnet new console--output <DIR>, dotnet sln add <DIR>, dotnet build

---
+++ aulapratica_2/teste/Program.cs
@@ -0,0 +1,9 @@
+﻿var o = new Octopus();    // Criação de um objeto do tipo Octopus
+
+Console.WriteLine(o.Age); // 10
+
+class Octopus
+{
+  string name;            // Atributo do tipo string, sem valor
+  public int Age = 10;    // Atributo do tipo int, com valor
+}
