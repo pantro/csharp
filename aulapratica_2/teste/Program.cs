82224c3ed0268d305018d55678c891c28cc17665
Gian Franco Joel Condori Luna <msft24@cervantes.LABS-IC>
2022-09-03 10:59:29 -0300

    poduce error de compilacion por que es un atributo de solo lectura

--- aulapratica_2/teste/Program.cs
+++ aulapratica_2/teste/Program.cs
@@ -1,9 +1,15 @@
-﻿var o = new Octopus();    // Criação de um objeto do tipo Octopus
+﻿var o = new Octopus("Jack");
 
-Console.WriteLine(o.Age); // 10
+Console.WriteLine(o.Name);  // Jack  
+o.Legs = 20;                // Erro de compilação
 
 class Octopus
 {
-  string name;            // Atributo do tipo string, sem valor
-  public int Age = 10;    // Atributo do tipo int, com valor
+  public readonly string Name;
+  public readonly int Legs = 8;
+  
+  public Octopus(string name)
+  {
+    Name = name;
+  }
 }
